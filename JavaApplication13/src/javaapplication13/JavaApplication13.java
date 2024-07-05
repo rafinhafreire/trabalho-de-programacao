23dcaab1abc38f6bc5c9e009af1280404f203bce
wuva <133061732+rafinhafreire@users.noreply.github.com>
2024-07-05 17:54:02 -0300

    JavaApplication13.java

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication13;
 
-/**
- *
- * @author Rafaela
- */
+ //questao 5
 public class JavaApplication13 {
 
     /**
